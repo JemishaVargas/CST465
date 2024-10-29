ddd6b64c41090536e9df8b7f24db2d04efe810d0
JemishaV <jemishavargas@gmail.com>
2024-10-28 22:33:05 -0700

    All except the forms

--- Assignment 2/Controllers/HomeController.cs
+++ Assignment 2/Controllers/HomeController.cs
@@ -2,12 +2,25 @@ public class HomeController : Controller
 
 {
 
+    [Route("")]
     public IActionResult Index() 
 
     {
-
+        ViewData["Title"] = "Home";
          return View(); 
 
+    }
+    [Route("ContactHTML")]
+    public IActionResult ContactHTML()
+    {
+        ViewData["Title"] = "ContactHTML";
+        return View();
+    }
+    [Route("ContactTagHelper")]
+    public IActionResult ContactTagHelper()
+    {
+        ViewData["Title"] = "ContactTagHelper";
+        return View();
     }
 
 }
